2a68acbb48f5fcaa29f8e6159f7a15c565c46af1
Filipe <filipe.coris14@gmail.com>
2023-05-14 16:24:07 +0100

    added command

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,54 @@
+import util.Command;
+
+import java.util.Scanner;
+
 /**
  * Entrypoint of the CineReviews application
  * @author Filipe Corista / João Rodrigues
  */
 public class Main {
+
+    // Constant Message
+    private static final String BYE = "Bye!";
+    private static final String LIST_COMMAND = "%s - %s\n";
+    private static final String UNKNOWN_COMMAND = "Unknown command. Type help to see available commands.";
+
     public static void main(String[] args) {
-        
+        executeCommands();
+    }
+
+    private static void executeCommands() {
+        Scanner in = new Scanner(System.in);
+
+        Command command;
+
+        do {
+            command = getCommand(in);
+            switch (command) {
+                case HELP -> executeHelp();
+                case EXIT -> System.out.println(BYE);
+                default -> System.out.println(UNKNOWN_COMMAND);
+            }
+        } while(!command.name().equals(Command.EXIT.name()));
+    }
+
+
+    private static void executeHelp() {
+        for (Command command : Command.values()) {
+            if (command != Command.UNKNOWN) {
+                String name = command.name().toLowerCase();
+                String description = command.getDescription();
+                System.out.printf(LIST_COMMAND, name, description);
+            }
+        }
+    }
+
+    private static Command getCommand(Scanner in) {
+        try {
+            String command = in.next().toUpperCase();
+            return Command.valueOf(command);
+        } catch (IllegalArgumentException e) {
+            return Command.UNKNOWN;
+        }
     }
 }
--- src/util/Command.java
+++ src/util/Command.java
@@ -1,2 +1,39 @@
-package util;public enum Commands {
+package util;
+
+/**
+ * Enum to store the commands of the application
+ * @author Filipe Corista / João Rodrigues
+ */
+public enum Command {
+    REGISTER("registers a user in the system"),
+    USERS("list all registered users"),
+    MOVIE("uploads a new movie"),
+    SERIES("uploads a new series"),
+    ARTIST("adds bio information about an artist"),
+    CREDITS("lists the bio and credits of an artist"),
+    REVIEWS("adds a review to a show"),
+    GENRE("lists the reviews of a show"),
+    RELEASED("lists shows released in a given year"),
+    AVOIDERS("lists artists that have no common projects"),
+    FRIENDS("lists artists that have more projects together"),
+    HELP("shows the available commands"),
+    EXIT("terminates the execution of the program"),
+    UNKNOWN("unknown command");
+
+    /**
+     * Stores the description of a command
+     */
+    final String description;
+
+    Command(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Gets the description of a command
+     * @return the description of a command
+     */
+    public String getDescription() {
+        return description;
+    }
 }
